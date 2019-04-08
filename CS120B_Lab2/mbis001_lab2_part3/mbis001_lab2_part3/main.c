f4a9691fd9a619b1d78f25014a1b272c96957172
MarkBis415 <mbis001@ucr.edu>
2019-04-07 23:29:28 -0700

    extension of part 2 (part 3)
    works only if part 2 works as well since the same arithmetic is used

--- CS120B_Lab2/mbis001_lab2_part3/mbis001_lab2_part3/main.c
+++ CS120B_Lab2/mbis001_lab2_part3/mbis001_lab2_part3/main.c
@@ -11,8 +11,32 @@
 int main(void)
 {
     /* Replace with your application code */
-    while (1) 
-    {
-    }
+    	DDRA = 0x00; PORTA = 0xFF;
+    	DDRC = 0xFF; PORTC = 0x00
+    	
+    	unsigned char cntavail = 0x00;
+    	unsigned char parkingSensor0 = 0x00;
+    	unsigned char parkingSensor1 = 0x00;
+    	unsigned char parkingSensor2 = 0x00;
+    	unsigned char parkingSensor3 = 0x00;
+    	
+    	while (1)
+    	{
+	    	parkingSensor0 = PINA & 0x01; //to see if bit 1 is available
+	    	parkingSensor1 = PINA & 0x02; // ' ' ' bit 2 ' '
+	    	parkingSensor2 = PINA & 0x04; // ' ' ' bit 3 ' '
+	    	parkingSensor3 = PINA & 0x08; // ' ' ' bit 4 ' '
+	    	
+	    	cntavail = parkingSensor0 + (parkingSensor1 >> 1) + (parkingSensor2 >> 2) + (parkingSensor3 >> 3)
+	    	if(cntavail == 0x04){
+				PORTC = 0x80;
+			}
+			else{
+				cntavail = parkingSensor0 + parkingSensor1 + parkingSensor2 + parkingSensor3;
+				cntavail = (~cntavail) & 0x0F;
+				PORTC = cntavail;
+			}
+    	}
+    	return 0;
 }
 
